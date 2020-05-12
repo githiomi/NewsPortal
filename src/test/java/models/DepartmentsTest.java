15bbe194e0f5b2f2613fea19c7cf9d15ff9ac31a
Dhosio <danielgithiomi@gmail.com>
2020-05-12 19:46:03 +0300

    Department Class setup and primary tests passed

--- src/test/java/models/DepartmentsTest.java
+++ src/test/java/models/DepartmentsTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +10,20 @@ public class DepartmentsTest {
 
     @Before
     public void setUp() throws Exception {
+    }
+
+    @Test
+    public void canCreateAnInstanceOfDepartment() {
+        Departments departments = new Departments("Management");
+        assertTrue(departments instanceof Departments);
+    }
+
+    @Test
+    public void checksIfTwoInstancesAreSimilar() {
+        Departments departments = new Departments("Management");
+        Departments departments1 = new Departments("Management");
+
+        assertTrue(departments.equals(departments1));
     }
 
     @After
