3119248a866add60802f946633be2cee2b34f196
Dhosio <danielgithiomi@gmail.com>
2020-05-13 11:39:40 +0300

    Landing page allows user to login with password

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -24,16 +24,33 @@ public class App {
         Connection conn;
         Gson gson = new Gson();
 
-        String connectionString = "jdbc:postgresql://localhost:4567/newsportal";
+        String connectionString = "jdbc:postgresql://localhost:5432/newsportal";
         Sql2o sql2o = new Sql2o(connectionString, "dhosio", "MaFaD@niel2019");
         newsDao = new Sql2oNews(sql2o);
         depNewsDao = new Sql2oDepNews(sql2o);
         usersDao = new Sql2oUsers(sql2o);
         conn = sql2o.open();
 
+        post("/home", "application/json", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String username = req.queryParams("username");
+                req.session().attribute("username", username);
+            int password = Integer.parseInt(req.queryParams("password"));
+
+            if (password != 123456) {
+                res.redirect("/");
+                model.put("username", req.session().attribute("username"));
+            }
+
+            model.put("username", req.session().attribute("username"));
+            return new ModelAndView(model, "home.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
         get("/", "application/json", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
 
+            model.put("username", req.session().attribute("username"));
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
